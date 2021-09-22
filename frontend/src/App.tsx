8052a1c9d2b3bbc27d60c3f9bfd34035b4e30393
lexpierini <alex_pierini@hotmail.com>
2021-09-21 21:04:20 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
